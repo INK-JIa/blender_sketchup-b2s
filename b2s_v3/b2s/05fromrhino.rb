139bbae4a8e1b5e7ef1b8aa38b61cb10f9c82f40
Jiaxiaokun <897542501@qq.com>
2024-11-12 10:40:21 +0800

    Delete b2s_v3 directory

--- b2s_v3/b2s/05fromrhino.rb
+++
@@ -1,8 +0,0 @@
-def from_rhino
- puts "Execute From Blender functionality."
- model = Sketchup.active_model
- options = { :units => "model",
-            :merge_coplanar_faces => true,
-            :show_summary => true }
- status = model.import("blendsu/fromrhino.glb", options)
-end
